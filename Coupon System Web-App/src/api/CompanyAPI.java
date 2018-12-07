a7f736a2df7f2683840d6274895946d8500cb7f7
Hagai Ben David <hagaibenda@gmail.com>
2018-12-08 00:00:06 +0200

    CompanyAPI

--- Coupon System Web-App/src/api/CompanyAPI.java
+++ Coupon System Web-App/src/api/CompanyAPI.java
@@ -1,6 +1,10 @@
 package api;
 
+import java.util.Collection;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -9,13 +13,14 @@ import javax.ws.rs.core.MediaType;
 
 import core.beans.CompanyBean;
 import core.beans.CustomerBean;
+import core.controller.CompanyController;
 import core.exception.CouponSystemException;
 
 @Path("/company")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CompanyAPI {
-	
+	private CompanyController companyController = new CompanyController();
 
 	
 	
@@ -27,67 +32,71 @@ public class CompanyAPI {
 	 *  If company creation fails
 	 */
 	@POST
-	public String createCompany(CompanyBean company) throws CouponSystemException {
-		System.out.println(company);
-		return new String ("hello");
+	public void createCompany(CompanyBean company) throws CouponSystemException {
+		companyController.createCompany(company);
 	}
+	
+
+
+
+	/**
+	 * Updates a company's details in the database
+	 * @param company Company to remove from the DB
+	 * @throws AdminFacadeException
+	 *  If given company's details don't match the details of the company with the same ID in the DB
+	 *  If updating of the company's details fails
+	 */
 	@PUT
-	public String createCompany(CustomerBean company) throws CouponSystemException {
-		System.out.println(company);
-		return new String ("hello");
+	public void updateCompany(CompanyBean company) throws CouponSystemException {
+		companyController.updateCompany(company);
+	}
+
+	
+	/**
+	 * Deletes a company in the database
+	 * -removes its coupons from DB (all tables)
+	 * @param companyId Company to remove from the DB
+	 * @throws AdminFacadeException
+	 *  If given company's details don't match the details of the company with the same ID in the DB
+	 *  If company deletion fails
+	 */
+	@DELETE
+	public void removeCompany(long companyId) throws CouponSystemException {
+		companyController.removeCompany(companyId);
 	}
 
 
-//	/**
-//	 * Updates a company's details in the database
-//	 * @param company Company to remove from the DB
-//	 * @throws AdminFacadeException
-//	 *  If given company's details don't match the details of the company with the same ID in the DB
-//	 *  If updating of the company's details fails
-//	 */
-//	public void updateCompany(CompanyBean company) throws CouponSystemException {
-//	}
-//
-//	
-//	/**
-//	 * Deletes a company in the database
-//	 * -removes its coupons from DB (all tables)
-//	 * @param companyId Company to remove from the DB
-//	 * @throws AdminFacadeException
-//	 *  If given company's details don't match the details of the company with the same ID in the DB
-//	 *  If company deletion fails
-//	 */
-//	public void removeCompany(long companyId) throws CouponSystemException {
-//	}
-//
-//
-//	
-//	/**
-//	 * Gets a company's details from the database
-//	 * 
-//	 * @param companyID ID of company to retrieve from the DB
-//	 * @return Company the company with the matching id
-//	 * @throws AdminFacadeException
-//	 *  If there is a connection problem or an SQLException is thrown to the DAO.
-//	 *  If the given company's ID can't be found in the DB (0 rows were returned).
-//	 */
-//	public CompanyBean getCompany(long companyID) throws CouponSystemException {	
-//		return companyDAO.getCompany(companyID);
-//	}
-//
-//
-//	
-//	/**
-//	 * Assemble and return an <code>ArrayList</code> of all the companies in the DB.
-//	 *
-//	 * @return An <code>ArrayList</code> of all the companies in DB.
-//	 * @throws AdminFacadeException If there is a connection problem or an <code>SQLException</code> is thrown to the DAO.
-//	 */
-//	public Collection<CompanyBean> getAllCompanies() throws CouponSystemException{
-//		return companyDAO.getAllCompanies();
-//	}
-//
-//
+	
+	/**
+	 * Gets a company's details from the database
+	 * 
+	 * @param companyID ID of company to retrieve from the DB
+	 * @return Company the company with the matching id
+	 * @throws AdminFacadeException
+	 *  If there is a connection problem or an SQLException is thrown to the DAO.
+	 *  If the given company's ID can't be found in the DB (0 rows were returned).
+	 */
+	@GET
+	public CompanyBean getCompany(long companyID) throws CouponSystemException {	
+		return companyController.getCompany(companyID);
+	}
+
+	
+
+	
+	/**
+	 * Assemble and return an <code>ArrayList</code> of all the companies in the DB.
+	 *
+	 * @return An <code>ArrayList</code> of all the companies in DB.
+	 * @throws AdminFacadeException If there is a connection problem or an <code>SQLException</code> is thrown to the DAO.
+	 */
+	@Path("companies")
+	@GET
+	public Collection<CompanyBean> getAllCompanies() throws CouponSystemException{
+		return companyController.getAllCompanies();
+	}
+
+
 //	/**
 //	 * Logs in to the coupon system as a specific company.
 //	 * @param name Company username
@@ -95,9 +104,10 @@ public class CompanyAPI {
 //	 * @return a new CompanyFacade instance if company's username and password are correct; otherwise, throws {@link CompanyController}
 //	 * @throws CompanyFacadeException if username or password are incorrect
 //	 */
+//	
 //	public long companyLogin(String name, String password) throws CouponSystemException {
 //		return companyDAO.companyLogin(name, password);
 //	}
-//
+
 
 }
