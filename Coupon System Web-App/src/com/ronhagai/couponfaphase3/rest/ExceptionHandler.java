90a9f53ecf22a7bd20fc9401ef3801a8a0170cbe
Hagai Ben David <hagaibenda@gmail.com>
2018-12-25 22:44:17 +0200

    Documentation exception handler

--- Coupon System Web-App/src/com/ronhagai/couponfaphase3/rest/ExceptionHandler.java
+++ Coupon System Web-App/src/com/ronhagai/couponfaphase3/rest/ExceptionHandler.java
@@ -13,6 +13,11 @@ import com.ronhagai.couponfaphase3.core.beans.ExceptionBean;
 import com.ronhagai.couponfaphase3.core.exception.CouponSystemException;
 import com.ronhagai.couponfaphase3.core.exception.ExceptionsEnum;
 
+/**
+ * Exception handler for all the Coupon System application exceptions
+ * @author hagai
+ *
+ */
 @Provider
 public class ExceptionHandler implements ExceptionMapper<Throwable> {
 	@Context
@@ -26,67 +31,79 @@ public class ExceptionHandler implements ExceptionMapper<Throwable> {
 		String internalMessage;
 		int statusCode;
 		
-		
+		//all the known exceptions hendeld by type
 		if (exception instanceof CouponSystemException) {
 			CouponSystemException theException = (CouponSystemException)exception;
+			//get the data from the exception.
 			exceptionEnum = theException.getExceptionsEnum();
 			statusCode = exceptionEnum.getStatusCode();
 			internalMessage = exceptionEnum.getInternalMessage();
 			
+			//OPERATION ERRORS
+			// OPERATIONS THAT SHLDNT NORMALLY FAIL
+			//dml equals 0
 			if (statusCode >= 1600 && statusCode < 1700) {
-				
-				// TODO appropriate handling, log
-				// OPERATIONS THAT SHLDNT NORMALLY FAIL
-				//dml equals 0
-			}
+			// TODO appropriate handling, log
+			}else
+			//EXTERNAL/DB ERRORS
+			// EXTERNAL FACTORS SHLDNT BE MORE THAN OCCASIONALY DEPENDING ON OTHERS' STABILLITY
 			if (statusCode >= 1700 && statusCode < 1800) {
 				// TODO appropriate handling, log 
-				// EXTERNAL FACTORS SHLDNT BE MORE THAN OCCASIONALY DEPENDING ON OTHERS' STABILLITY
-			}
+			}else
+			// CLIENT SIDE ERRORS WITH HIGHER OCCURENCES 
 			if (statusCode >= 1800 && statusCode < 1900) {
-				// CLIENT SIDE ERRORS WITH HIGHER OCCURENCES 
-			}
-			if (statusCode >= 1900 && statusCode < 2000) {
+				
+			}else
 				// SECURITY ERRORS HIGH CHANCES OF BREACH ATTEMPTS
 				// HIDES THE TRUE CAUSE
+			if (statusCode >= 1900 && statusCode < 2000) {
 				exceptionEnum = ExceptionsEnum.DATA_BASE_ERROR;
 				statusCode = exceptionEnum.getStatusCode();
 				internalMessage = exceptionEnum.getInternalMessage();
-			}
+			}else
+			// CRITICAL ERRORS NOTIFY IMMIDIATLY
 			if (statusCode >= 2000) {
-				// TODO send email to manager +log
-				// CRITICAL ERRORS NOTIFY IMMIDIATLY
+				// TODO send email to manager + log
 				exception.printStackTrace();
 			}
 		}else {
-			//TODO Log uncaught exception
+			//handle the uncaught exceptions by sending general message to the clint and print to log
+			//TODO Log uncaught exceptions
 			System.err.println("Different exception than CouponSystemException: ");
 			exception.printStackTrace();
 			exceptionEnum = ExceptionsEnum.FAILED_OPERATION;
 			statusCode = exceptionEnum.getStatusCode();
 			internalMessage = exceptionEnum.getInternalMessage();
 		}
+		///set the message to user and for client developer
 		externalMessage = getTranslatedMessage(exceptionEnum);
-		externalMessage = exception.getMessage();//TODO only for developing purposes 
+		//TODO only for developing purposes!!!! 
+		externalMessage = exception.getMessage();
 		exceptionBean = new ExceptionBean(statusCode, externalMessage, internalMessage);
 		return Response.status(statusCode).entity(exceptionBean).build();	
 	}
 	
+	//set the message that the user will see to his language 
 	private String getTranslatedMessage(ExceptionsEnum exceptionEnum) {
 		ResourceBundle errorMessages;
+		//get the user language 
 		Locale locale = request.getLocale();
 		try {
+			//get the right translated file for user language 
 			errorMessages = ResourceBundle.getBundle("com.ronhagai.couponfaphase3.core.exception.errorMessages", locale);			
 		} catch (Exception e1) {
 			// TODO LOG IN TRANSLATION LOG
+			//if ther isent a  file for the ask language set en to be the difolt language 
 			System.err.println(String.format("%s does not have error language support", request.getLocale().getDisplayLanguage()));
 			locale = new Locale("en");
 			errorMessages = ResourceBundle.getBundle("com.ronhagai.couponfaphase3.core.exception.errorMessages", locale);			
 		}
 		try {
+			//return the translate message
 			return errorMessages.getString(exceptionEnum.name());
 		} catch (Exception e1) {
 			// TODO LOG IN TRANSLATION LOG
+			//if a translated message dosent  exist return as difolt the english one
 			System.err.println(String.format("Error code %s does not have support for %s", String.valueOf(exceptionEnum.getStatusCode()), locale.getDisplayLanguage()));
 			errorMessages = ResourceBundle.getBundle("com.ronhagai.couponfaphase3.core.exception.errorMessages_en");
 			try {
