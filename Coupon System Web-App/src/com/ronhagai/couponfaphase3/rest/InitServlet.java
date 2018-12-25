bedc0e76d008ce7fc70952e30fb5dc3473b1770e
RED_DWARF <roniedery1@hotmail.com>
2018-12-25 18:56:25 +0200

    initservlet

--- Coupon System Web-App/src/com/ronhagai/couponfaphase3/rest/InitServlet.java
+++ Coupon System Web-App/src/com/ronhagai/couponfaphase3/rest/InitServlet.java
@@ -1,17 +1,10 @@
 package com.ronhagai.couponfaphase3.rest;
 
-import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.ronhagai.couponfaphase3.core.CouponSystem;
 
-/**
- * Servlet implementation class SystemServlet
- */
 
 public class InitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -21,13 +14,7 @@ public class InitServlet extends HttpServlet {
 	private static String databaseUrl;// = "jdbc:mysql://db4free.net:3306/coupon_system";
 	private static String userName;// = "coupon_group";
 	private static String password;// = "12345678";
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InitServlet() {
-        super();		
-        // TODO Auto-generated constructor stub
-    }
+
 
 	@Override
 	public void destroy() {
@@ -44,25 +31,6 @@ public class InitServlet extends HttpServlet {
 		databaseUrl = getInitParameter("databaseUrl");
 		userName = getInitParameter("userName");
 		password = getInitParameter("password");
-//		System.out.println(driverName+databaseUrl+userName+password);
-		couonSystem.setServer(driverName, databaseUrl, userName, password);
-		
+		couonSystem.setServer(driverName, databaseUrl, userName, password);	
 	}
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
